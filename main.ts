cdc7bce0df5ee1bdd7a1b2956ffa552799a7bd22
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-02-29 22:31:09 +0100

    Mise en place du menu

--- main.ts
+++ main.ts
@@ -1,7 +1,47 @@
 radio.setGroup(69);
-let pause_play = true;
+let pause_play = false;
+let menu = true;
+
+while (menu) {
+    let pin3 = analog2digital(pins.analogReadPin(AnalogPin.P3));
+    let pin10 = analog2digital(pins.analogReadPin(AnalogPin.P10));
+    let pin1 = analog2digital(pins.analogReadPin(AnalogPin.P1))
+    let pin2 = analog2digital(pins.analogReadPin(AnalogPin.P2));
+    let side = 'R';
+
+    if (pin3 && pin10) {
+        radio.sendString(side + "_array_N");
+    }
+    else if (pin3 && pin2) {
+        radio.sendString(side + "_array_S");
+    }
+    else if (pin1 && pin2) {
+        radio.sendString(side + "_choix");
+    }
+}
+
+
+
+
+
+
 input.onPinPressed(TouchPin.P0 && TouchPin.P1, function () {
-    if (pause_play) {
+    if (pause) {
+        radio.sendString("R_play_");
+        basic.pause(3200);
+        testingKeys();
+    }
+})
+
+radio.onReceivedString(function (receivedString: string) {
+    if (receivedString.substr(2, 6) == "pause") {
+        pause_play = true;
+    }
+    else if (receivedString.substr(2, 6) == "play_") {
+        pause_play = false;
+    }
+    else if (receivedString == "start_game") {
+        menu = false;
         testingKeys();
     }
 })
