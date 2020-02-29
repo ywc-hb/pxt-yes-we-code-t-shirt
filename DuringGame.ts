1cce149d8468f53ad9910405520fdb9eaa3a22fe
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-02-29 21:23:52 +0100

    Update main.ts, DuringGame.ts

--- DuringGame.ts
+++ DuringGame.ts
@@ -1,1 +1,72 @@
-// Ajouter votre code ici
+function testingKeys () {
+    let side = "R";
+    while (true) {
+        let pin0: boolean = analog2digital(pins.analogReadPin(AnalogPin.P0));
+        let pin1: boolean = analog2digital(pins.analogReadPin(AnalogPin.P1));
+        let pin2: boolean = analog2digital(pins.analogReadPin(AnalogPin.P2));
+        let pin3: boolean = analog2digital(pins.analogReadPin(AnalogPin.P3));
+        let pin4: boolean = analog2digital(pins.analogReadPin(AnalogPin.P4));
+        let pin10: boolean = analog2digital(pins.analogReadPin(AnalogPin.P10));
+
+        //Test des flèches directrices
+        if (!(pin1 && pin2)) {
+            if (pin3 && pin4) {
+                radio.sendString(side + "_move_W");
+            }
+            else if (pin3 && pin1) {
+                radio.sendString(side + "_move_E");
+            }
+            else if (pin3 && pin10) {
+                radio.sendString(side + "_move_N");
+            }
+            else if (pin3 && pin2) {
+                radio.sendString(side + "_move_S");
+            }
+            else if (pin0 && pin1) {
+                radio.sendString(side + "_move_NE");
+            }
+            else if (pin0 && pin10) {
+                radio.sendString(side + "_move_NW");
+            }
+            else if (pin0 && pin2) {
+                radio.sendString(side + "_move_SE");
+            }
+            else if (pin4 && pin10) {
+                radio.sendString(side + "_move_SW");
+            }
+        }
+
+        else if (pin1 && pin2) {
+            if (pin3 && pin4) {
+                radio.sendString(side + "_shoot_W");
+            }
+            else if (pin3 && pin1) {
+                radio.sendString(side + "_shoot_E");
+            }
+            else if (pin3 && pin10) {
+                radio.sendString(side + "_shoot_N");
+            }
+            else if (pin3 && pin2) {
+                radio.sendString(side + "_shoot_S");
+            }
+            else if (pin0 && pin1) {
+                radio.sendString(side + "_shoot_NE");
+            }
+            else if (pin0 && pin10) {
+                radio.sendString(side + "_shoot_NW");
+            }
+            else if (pin0 && pin2) {
+                radio.sendString(side + "_shoot_SE");
+            }
+            else if (pin4 && pin10) {
+                radio.sendString(side + "_shoot_SW");
+            }
+        }
+        else if (pin1 && pin10) {
+            radio.sendString(side + "_charg")
+        }
+
+        basic.pause(100);
+    }
+}
+ 
