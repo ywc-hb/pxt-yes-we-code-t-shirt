1441d1c74e87695baa643c2594970253f51d6e30
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-02-29 20:41:44 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,23 @@
-radio.setGroup(69)
+radio.setGroup(69);
 
+function analog2digital (pin: number): boolean {
+    let limit: number = 600;
+    if (pin > limit) {
+        return (true);
+    }
+    else {
+        return (false);
+    }
+}
+
+
+while (true) {
+    let pin0: boolean = analog2digital(pins.analogReadPin(AnalogPin.P0));
+    let pin1: boolean = analog2digital(pins.analogReadPin(AnalogPin.P1));
+    let pin2: boolean = analog2digital(pins.analogReadPin(AnalogPin.P2));
+    let pin3: boolean = analog2digital(pins.analogReadPin(AnalogPin.P3));
+    let pin4: boolean = analog2digital(pins.analogReadPin(AnalogPin.P4));
+    let pin10: boolean = analog2digital(pins.analogReadPin(AnalogPin.P10));
+
+    
+}
