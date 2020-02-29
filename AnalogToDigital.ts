ac7629d383e68db80810081f572ec66ad9416628
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-02-29 21:19:17 +0100

    Update AnalogToDigital.ts

--- AnalogToDigital.ts
+++ AnalogToDigital.ts
@@ -1,1 +1,9 @@
-// Ajouter votre code ici
+function analog2digital (pin: number): boolean {
+    let limit: number = 600;
+    if (pin > limit) {
+        return (true);
+    }
+    else {
+        return (false);
+    }
+}
