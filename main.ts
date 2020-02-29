8c1689ceb6634cb2b6a241121d413df66087654a
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-02-29 22:34:54 +0100

    Organisation du code

--- main.ts
+++ main.ts
@@ -2,23 +2,7 @@ radio.setGroup(69);
 let pause_play = false;
 let menu = true;
 
-while (menu) {
-    let pin3 = analog2digital(pins.analogReadPin(AnalogPin.P3));
-    let pin10 = analog2digital(pins.analogReadPin(AnalogPin.P10));
-    let pin1 = analog2digital(pins.analogReadPin(AnalogPin.P1))
-    let pin2 = analog2digital(pins.analogReadPin(AnalogPin.P2));
-    let side = 'R';
 
-    if (pin3 && pin10) {
-        radio.sendString(side + "_array_N");
-    }
-    else if (pin3 && pin2) {
-        radio.sendString(side + "_array_S");
-    }
-    else if (pin1 && pin2) {
-        radio.sendString(side + "_choix");
-    }
-}
 
 
 input.onPinPressed(TouchPin.P0 && TouchPin.P1, function () {
