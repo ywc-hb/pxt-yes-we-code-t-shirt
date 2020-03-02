930fb7a55250e54e2b756f0a5c5beeb65d932ef3
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-03-02 18:42:36 +0100

    Mise à jour DuringGame.ts

--- DuringGame.ts
+++ DuringGame.ts
@@ -1,5 +1,4 @@
 function testingKeys () {
-    let side = "R";
     pause_play = false;
     while (!pause) {
         let pin0: boolean = analog2digital(pins.analogReadPin(AnalogPin.P0));
@@ -12,64 +11,64 @@ function testingKeys () {
         // Voir la correspondance des pins sur le github
         if (!(pin1 && pin2)) {
             if (pin3 && pin4) {
-                radio.sendString(side + "_move_W");
+                radio.sendString("R_move_W");
             }
             else if (pin3 && pin1) {
-                radio.sendString(side + "_move_E");
+                radio.sendString("R_move_E");
             }
             else if (pin3 && pin10) {
-                radio.sendString(side + "_move_N");
+                radio.sendString("R_move_N");
             }
             else if (pin3 && pin2) {
-                radio.sendString(side + "_move_S");
+                radio.sendString("R_move_S");
             }
             else if (pin0 && pin1) {
-                radio.sendString(side + "_move_NE");
+                radio.sendString("R_move_NE");
             }
             else if (pin0 && pin10) {
-                radio.sendString(side + "_move_NW");
+                radio.sendString("R_move_NW");
             }
             else if (pin0 && pin2) {
-                radio.sendString(side + "_move_SE");
+                radio.sendString("R_move_SE");
             }
             else if (pin4 && pin10) {
-                radio.sendString(side + "_move_SW");
+                radio.sendString("R_move_SW");
             }
         }
 
         else if (pin1 && pin2) {
             if (pin3 && pin4) {
-                radio.sendString(side + "_shoot_W");
+                radio.sendString("R_shoot_W");
             }
             else if (pin3 && pin1) {
-                radio.sendString(side + "_shoot_E");
+                radio.sendString("R_shoot_E");
             }
             else if (pin3 && pin10) {
-                radio.sendString(side + "_shoot_N");
+                radio.sendString("R_shoot_N");
             }
             else if (pin3 && pin2) {
-                radio.sendString(side + "_shoot_S");
+                radio.sendString("R_shoot_S");
             }
             else if (pin0 && pin1) {
-                radio.sendString(side + "_shoot_NE");
+                radio.sendString("R_shoot_NE");
             }
             else if (pin0 && pin10) {
-                radio.sendString(side + "_shoot_NW");
+                radio.sendString("R_shoot_NW");
             }
             else if (pin0 && pin2) {
-                radio.sendString(side + "_shoot_SE");
+                radio.sendString("R_shoot_SE");
             }
             else if (pin4 && pin10) {
-                radio.sendString(side + "_shoot_SW");
+                radio.sendString("R_shoot_SW");
             }
         }
 
         else if (pin1 && pin10) {
-            radio.sendString(side + "_charg");
+            radio.sendString("R_charg");
         }
 
         else if (pin4 && pin2) {
-            radio.sendString(side + "_pause");
+            radio.sendString("R_pause");
             pause_play = true;
         }
 
