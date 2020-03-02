63f9677105f89728a7cf21dac97226c0a64abe4b
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-03-02 18:32:38 +0100

    Mise à jour ReviveCode.ts

--- ReviveCode.ts
+++ ReviveCode.ts
@@ -32,9 +32,9 @@ class ReviveCode {
 
     protected testingReviveCode(): boolean {
         let error = false;
+        let touched: (ArrowNames | string)[];
 
-        while (!pause) {
-            let touched: (ArrowNames | string)[];
+        while (touched.length < this.code.length) {
             let pin0: boolean = analog2digital(pins.analogReadPin(AnalogPin.P0));
             let pin1: boolean = analog2digital(pins.analogReadPin(AnalogPin.P1));
             let pin2: boolean = analog2digital(pins.analogReadPin(AnalogPin.P2));
@@ -43,8 +43,45 @@ class ReviveCode {
             let pin10: boolean = analog2digital(pins.analogReadPin(AnalogPin.P10));
 
             // Voir la correspondance des pins sur le github
-            if (pin)
+            if (pin3 && pin4) {
+                touched.push(ArrowNames.West);
+            }
+            if (pin3 && pin1) {
+                touched.push(ArrowNames.East);
+            }
+            if (pin3 && pin10) {
+                touched.push(ArrowNames.North);
+            }
+            if (pin3 && pin2) {
+                touched.push(ArrowNames.South);
+            }
+            if (pin0 && pin10) {
+                touched.push(ArrowNames.NorthWest);
+            }
+            if (pin10 && pin4) {
+                touched.push(ArrowNames.SouthWest);
+            }
+            if (pin0 && pin1) {
+                touched.push(ArrowNames.NorthEast);
+            }
+            if (pin2 && pin4) {
+                touched.push(ArrowNames.SouthEast);
+            }
+            if (pin1 && pin2) {
+                touched.push("T");
+            }
+            if (pin1 && pin10) {
+                touched.push("R");
+            }
+            
             basic.pause(100);
+        }
+
+        for (let i = 0; i < this.code.length; i++) {
+            if (touched[i] != this.code[i]) {
+                error = true;
+                break;
+            }
         }
 
         if (error) {
